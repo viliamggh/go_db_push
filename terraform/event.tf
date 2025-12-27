0e8fdc2f82b383337b54db663ba6e111e0b51b44
Viliam Gago <viliam.gago95@gmail.com>
2025-12-27 20:16:41 +0100

    Add Event Grid subscription for blob created events

---
+++ terraform/event.tf
@@ -0,0 +1,17 @@
+# Event Grid subscription for blob created events
+resource "azurerm_eventgrid_event_subscription" "blob_created" {
+  name  = "blob-created-to-db-push"
+  scope = local.storage_account_id
+
+  webhook_endpoint {
+    url = "https://${azurerm_container_app.db_push.latest_revision_fqdn}/blobCreated"
+  }
+
+  included_event_types = ["Microsoft.Storage.BlobCreated"]
+
+  subject_filter {
+    subject_begins_with = "/blobServices/default/containers/${local.storage_container_name}/blobs/"
+  }
+
+  depends_on = [azurerm_container_app.db_push]
+}
